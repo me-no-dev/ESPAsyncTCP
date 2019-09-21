f912a82b3b4517e4e67f10441bf0662ab96e6229
me-no-dev <hristo@espressif.com>
2019-09-21 14:20:32 +0300

    Update build-pio.sh

--- travis/build-pio.sh
+++ travis/build-pio.sh
@@ -6,7 +6,7 @@ if [ $? -ne 0 ]; then exit 1; fi
 echo -e "travis_fold:end:install_pio"
 
 echo -e "travis_fold:start:install_lib"
-python -m platformio lib --storage-dir $TRAVIS_BUILD_DIR
+python -m platformio lib --storage-dir $TRAVIS_BUILD_DIR install
 if [ $? -ne 0 ]; then exit 1; fi
 echo -e "travis_fold:end:install_lib"
 
@@ -17,14 +17,20 @@ for EXAMPLE in $TRAVIS_BUILD_DIR/examples/*/*.ino; do
 done
 echo -e "travis_fold:end:test_pio"
 
-echo -e "travis_fold:start:test_web_server"
+echo -e "travis_fold:start:install_json"
 python -m platformio lib install https://github.com/bblanchon/ArduinoJson.git
 if [ $? -ne 0 ]; then exit 1; fi
+echo -e "travis_fold:end:install_json"
 
 cd $HOME/
+echo -e "travis_fold:start:install_web_server"
 git clone https://github.com/me-no-dev/ESPAsyncWebServer
 if [ $? -ne 0 ]; then exit 1; fi
+python -m platformio lib --storage-dir $HOME/ESPAsyncWebServer install
+if [ $? -ne 0 ]; then exit 1; fi
+echo -e "travis_fold:end:install_web_server"
 
+echo -e "travis_fold:start:test_web_server"
 for EXAMPLE in $HOME/ESPAsyncWebServer/examples/*/*.ino; do
     python -m platformio ci $EXAMPLE -l $TRAVIS_BUILD_DIR -b esp12e
 	if [ $? -ne 0 ]; then exit 1; fi
